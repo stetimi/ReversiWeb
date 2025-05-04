9c9a66b28ba8aa803896477fcb0965ea8293447b
stetimi <123815784+stetimi@users.noreply.github.com>
2025-05-04 21:14:45 +0800

    Highlight the Player 1 or Player 2 box with a green border according to which is the current player.

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -11,5 +11,9 @@ export interface PlayerProps {
 export const Player: React.FC<PlayerProps> = ({ piece, score, isActive }) => {
   const playerClass = piece === 'b' ? 'player-left' : 'player-right';
   const playerName = `Player ${piece === 'b' ? '1' : '2'}`;
-  return <div className={`player-info ${playerClass}`}>{playerName}</div>;
+  return (
+    <div className={`player-info ${playerClass} ${isActive ? 'active-player' : ''}`}>
+      {playerName}
+    </div>
+  );
 };
